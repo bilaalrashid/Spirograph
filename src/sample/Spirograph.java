7c31e09236b7abe9053b3a3ce4cd9ffac9e6a617
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-11-08 16:42:28 +0000

    Added a spirograph

---
+++ src/sample/Spirograph.java
@@ -0,0 +1,20 @@
+package sample;
+
+/**
+ * A spirograph
+ */
+class Spirograph {
+
+    private double R;
+
+    private double r;
+
+    private double O;
+
+    public Spirograph(double R, double r, double O) {
+        this.R = R;
+        this.r = r;
+        this.O = O;
+    }
+
+}
