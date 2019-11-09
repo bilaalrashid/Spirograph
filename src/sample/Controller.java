a7f664bec9976cb836f34244180885087bf59deb
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-11-09 12:52:11 +0000

    Added the canvas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,43 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * The controller for the main stage
+ */
+public class Controller implements Initializable {
+
+    /**
+     * The canvas to display the spirograph on
+     */
+    @FXML
+    private Canvas canvas;
+
+    /**
+     * The graphics context of the canvas to draw on
+     */
+    private GraphicsContext graphicsContext;
+
+    /**
+     * Initialises the graphics context from the canvas
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.graphicsContext = this.canvas.getGraphicsContext2D();
+    }
+
+    /**
+     * Draws the spirograph on the graphics context
+     */
+    @FXML
+    private void draw() {
+
+    }
+
 }
